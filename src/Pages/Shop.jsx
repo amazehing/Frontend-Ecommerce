008a64feb87908b6401725d8890dea6a6711f040
amazehing <amazehing@example.com>
2024-05-19 03:23:54 +0200

    Extra map verwijderd

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
-import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollection from '../Components/NewCollections/NewCollection'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
@@ -9,7 +8,6 @@ const Shop = () => {
   return (
     <div>
       <Hero/>
-      <Popular/>
       <Offers/>
       <NewCollection/>
       <NewsLetter/>
